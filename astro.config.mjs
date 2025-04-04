761bc09a29eaad1f7abddf3b9ff517af144e1358
Rodrigo Toledo <rodrigotoledo@worldlex.net>
2025-04-04 20:36:49 +0200

    Refactor astro.confi.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless'
+
 
 export default defineConfig({
   integrations: [react()],
   adapter: vercel(),
-  output: 'static',
+  output: 'server',
+  base: '/',
 });
