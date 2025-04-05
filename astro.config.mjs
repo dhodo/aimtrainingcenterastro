6c5456f60a0790e4cd4bcccd5c308924359c9420
Rodrigo Toledo <rodrigotoledo@worldlex.net>
2025-04-05 10:26:03 +0200

    next2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [react()],
+  output: 'server',
   adapter: vercel(),
-  output: 'static',
 });
